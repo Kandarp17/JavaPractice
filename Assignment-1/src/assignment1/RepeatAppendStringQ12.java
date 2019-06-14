62862bc24e55e18c0c0f2ad3223343c282840a77
Kandarp <patelkandarp342@gmail.com>
2019-06-14 14:02:51 -0400

    q12

--- Assignment-1/src/assignment1/RepeatAppendStringQ12.java
+++ Assignment-1/src/assignment1/RepeatAppendStringQ12.java
@@ -1,8 +1,19 @@
 package assignment1;
-
+/*
+ Repeat a String number of times and append it
+Ex: hello 4
+hellohellohellohello
+ */
 public class RepeatAppendStringQ12 {
 	public static void repeatAppendStr(String input,int num) {
-		StringBuffer result= new StringBuffer(input);
+		StringBuffer result= new StringBuffer();
+		for(int i=0;i<num;i++) {
+			result.append(input);
+		}
+		System.out.println("Appended String: "+result);
+	
 	}
-
+	public static void main(String[] args) {
+		repeatAppendStr("kandarp",4);
+	}
 }
